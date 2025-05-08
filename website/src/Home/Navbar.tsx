d8c569eceae87cf6b82e05fc37dd9e2a2484568f
BarsatKhadka <khadkabarsat598@gmail.com>
2025-05-07 20:35:34 -0500

    add: logo in navbar

--- website/src/Home/Navbar.tsx
+++ website/src/Home/Navbar.tsx
@@ -1,7 +1,9 @@
+import vinayaLogo from '../assets/vinayaJournal-removebg.png';
 export const Navbar = () => {
     return (
         <div className="flex flex-col pt-8 items-center h-screen">
-            <div className="max-w-6xl w-full p-4 rounded-2xl border border-gray-200 bg-white shadow-sm">
+            <div className="max-w-6xl w-full p-2 rounded-2xl border border-gray-200 bg-white shadow-sm flex items-center">
+                <img src={vinayaLogo} alt="Vinaya Journal Logo" className="h-20 " />
                 <p>VINAYA JOURNAL</p>
             </div>
         </div>
