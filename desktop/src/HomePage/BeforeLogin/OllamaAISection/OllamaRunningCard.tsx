a90c3b1d56cb214b02731a29cf5ac45331c09f39
BarsatKhadka <khadkabarsat598@gmail.com>
2025-05-13 19:27:36 -0500

    desktop - add: function to get all Ollama Models present if ollama is running

--- desktop/src/HomePage/BeforeLogin/OllamaAISection/OllamaRunningCard.tsx
+++ desktop/src/HomePage/BeforeLogin/OllamaAISection/OllamaRunningCard.tsx
@@ -1,6 +1,22 @@
+import { useEffect , useState } from "react";
+import axios from "axios";
+
 export const OllamaRunningCard = () => {
-    return (
+    const [ollamaModels, setOllamaModels] = useState<any[]>([]);
+    useEffect(() => {
+        const checkOllamaModels = async () => {
+            try {
+                const response = await axios.get("http://localhost:8000/models");
+                setOllamaModels(response?.data.models || []);
+            } catch (error) {
+                console.error("Error checking Ollama status:", error);
+            }
+        };
+        checkOllamaModels();
+    }, []);
+        return (
         <div>
+            {ollamaModels}
             Ollama AI is running.
         </div>
 
