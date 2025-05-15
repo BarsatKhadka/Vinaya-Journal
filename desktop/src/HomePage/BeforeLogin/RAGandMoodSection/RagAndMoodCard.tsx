6d16c6e30c69aecc70b4d132541ec78ef97d37d7
BarsatKhadka <khadkabarsat598@gmail.com>
2025-05-15 14:48:45 -0500

    desktop - add: structure between Rag and mood card component , half space each

--- desktop/src/HomePage/BeforeLogin/RAGandMoodSection/RagAndMoodCard.tsx
+++ desktop/src/HomePage/BeforeLogin/RAGandMoodSection/RagAndMoodCard.tsx
@@ -3,9 +3,13 @@ import { MoodCard } from "./MoodCard"
 
 export const RagAndMoodCard = () => { 
     return(
-        <div> 
-            <RagCard />
-            <MoodCard />
+        <div style={{ display: "flex" }}>
+            <div style={{ flex: 1 }}>
+                <RagCard />
+            </div>
+            <div style={{ flex: 1 }}>
+                <MoodCard />
+            </div>
         </div>
     )
 }
