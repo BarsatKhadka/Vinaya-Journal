4fb9916e5700cc6689bc1a00e422f886a2b9ae07
BarsatKhadka <khadkabarsat598@gmail.com>
2025-05-15 11:59:38 -0500

    desktop - add: frontend hook and await function to get os name

--- desktop/src/HomePage/BeforeLogin/OllamaAISection/OllamaNotRunningCard.tsx
+++ desktop/src/HomePage/BeforeLogin/OllamaAISection/OllamaNotRunningCard.tsx
@@ -1,8 +1,24 @@
+import {useEffect, useState} from "react";
+
 export const OllamaNotRunningCard = () => {
+    const [osName,setOsName] = useState<string>("");
+
+    useEffect(() =>{
+        const getOsName = async () => {
+            try{
+                const osName = await window.ipcRenderer.invoke("get-os")
+                setOsName(osName);
+            }
+            catch (error) {
+                console.error("Error Getting os name: ", error);
+            }   
+        };
+        getOsName();
+    }, [])
     
     return (
         <div>
-            Ollama AI is not running.
+            Ollama AI is not running. {osName}
         </div>
     );
 }
