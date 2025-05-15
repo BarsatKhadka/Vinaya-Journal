f07a78e85fd2ae79d135ca5e2b05df0898c0a6ce
BarsatKhadka <khadkabarsat598@gmail.com>
2025-05-15 16:56:39 -0500

    desktop - update: mood icon replace and image in moodcard

--- desktop/src/HomePage/BeforeLogin/RAGandMoodSection/MoodCard.tsx
+++ desktop/src/HomePage/BeforeLogin/RAGandMoodSection/MoodCard.tsx
@@ -1,7 +1,19 @@
+import MoodIcon from '../../../assets/FeatureCardIcons/MoodIcon.png'
+
 export const MoodCard = () => {
     return (
-        <>
-        This is mood card
-        </>
+        <div className="flex flex-col items-center  rounded-xl 
+                    border border-gray-400 shadow-sm transition hover:shadow-md"
+             style={{ backgroundColor: "#FCFBFA" }}>
+            
+            <div className="w-[50%]">
+                <img
+                    src={MoodIcon}
+                    alt="Mood tracking visualization"
+                    className="w-full h-full object-contain"
+                />
+            </div>
+
+        </div>
     );
-}
+};
