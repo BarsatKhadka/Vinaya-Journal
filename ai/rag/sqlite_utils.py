1f1c80cadd993dbf0647cd605bf5a27a660c0eb8
BarsatKhadka <khadkabarsat598@gmail.com>
2025-05-26 15:31:25 -0500

    ai -update: get_all_entries updated to give content date with entries

--- ai/rag/sqlite_utils.py
+++ ai/rag/sqlite_utils.py
@@ -4,10 +4,11 @@ from pathlib import Path
 user_home = Path.home()
 database_path = user_home / "vinayadb" / "journalEntries.db"
 
-def get_all_entries():
+def get_all_entries() -> list[tuple[str, str]]:
+    """Returns all entries with content date from the database"""
+    
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
-    cursor.execute("SELECT content FROM entries")
+    cursor.execute("SELECT entry_date,content FROM entries")
     rows = cursor.fetchall()
-    content = [i for i in rows]
-    print(content)
+    return rows
