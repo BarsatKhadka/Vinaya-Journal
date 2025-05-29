3ebacf99fa1862f50e21f50b24d9e981695cc3c3
BarsatKhadka <khadkabarsat598@gmail.com>
2025-05-29 14:40:14 -0500

    ai -add: create embeddings with all-MiniLM-L6-v2

--- ai/rag/embedder.py
+++ ai/rag/embedder.py
@@ -9,10 +9,13 @@ embedding_model = SentenceTransformer(model_name_or_path="all-MiniLM-L6-v2" , de
 
 
 def get_all_entries_embeddings():
-    # raw_entries = get_all_entries()
-    # content = [text for date,text in raw_entries if text.strip()]
-    # embeddings = embedding_model.encode(content)
-    return give_chunks_info()
+    raw_entries = get_all_entries()
+    chunks_info = give_chunks_info()
+    for date in chunks_info.keys():
+        embeddings = embedding_model.encode(chunks_info[date]["chunked_sentences"])
+        chunks_info[date]["embeddings"] = embeddings
+        print(embeddings.shape)
+    return chunks_info
     
 
 
