189f7094c621cb890b6909d4465338f00f49e06d
BarsatKhadka <khadkabarsat598@gmail.com>
2025-06-10 09:49:16 -0500

    Vercel's analytics

--- website/src/Home/components/BeginJournaling.tsx
+++ website/src/Home/components/BeginJournaling.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion"
 import { Github } from "lucide-react"
-import { Link } from "react-router-dom"
 import BeginJournalingImage from "../../assets/BeginJournalingPen.png"
 import ZeroCloudImage from "../../assets/FeatureCardIcons/CloudIcon.png"
 
